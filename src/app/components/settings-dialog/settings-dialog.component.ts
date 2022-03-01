fd59fbb6608dc03a2c7d27322bbea986c1077740
caeres <patrick.rubner97@gmail.com>
2022-03-01 23:35:09 +0100

    AHAHA FUCK YOU GOD IT TURNS BLUE

--- src/app/components/settings-dialog/settings-dialog.component.ts
+++ src/app/components/settings-dialog/settings-dialog.component.ts
@@ -3,15 +3,32 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { TimerStateService, TimerType } from 'src/app/services/timer-state.service';
 import { HeaderComponent } from '../header/header.component';
 import { FormControl, Validators } from '@angular/forms';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
+// animation fuckery lets go
 @Component({
   selector: 'app-settings-dialog',
   templateUrl: './settings-dialog.component.html',
-  styleUrls: ['./settings-dialog.component.scss']
+  styleUrls: ['./settings-dialog.component.scss'],
+  animations: [
+    trigger('hasErrors', [
+      state('nothingHappened', style({})),
+      state('errorFound', style({
+        backgroundColor: 'blue'
+      })),
+      transition('nothingHappened => errorFound', [
+        animate('2s')
+      ]), 
+      transition('errorFound => nothingHappened', [
+        animate('2s')
+      ])
+    ])
+  ]
 })
 export class SettingsDialogComponent implements OnInit {
 
-  
+
+  isfucked = false;
   // pretty sure a simple formGroup wouldve been possible as well but i cannot be arsed rn tbh
   pomoCheck = new FormControl('', [Validators.required, Validators.pattern('^([1-9]|[1-5][0-9]|60])$')]);
   shortCheck = new FormControl('', [Validators.required, Validators.pattern('^([1-9]|[1-5][0-9]|60])$')]);
@@ -26,8 +43,10 @@ export class SettingsDialogComponent implements OnInit {
   ) { }
   ngOnInit(): void {  }
 
+
+
   // same here: there prolly is a "one-funciton" solution via a formgroup or something but nah, this works :D
-get errorMessage() {
+  get errorMessage() {
   // Checks pomodoro settings field
   if (this.pomoCheck.hasError('required')) return this.errorMessageTextRequired;
   if (this.pomoCheck.hasError('pattern')) return this.errorMessageTextPattern;
@@ -41,23 +60,24 @@ get errorMessage() {
 }
 
   // on click with errors -> animation
-  onClickSave(pomoValue : string, shortValue : string, longValue : string): void {
-    if (this.errorMessage != "") {
-       console.log("There was at least one error")
-      //  angry wiggle soon
-     }
-    else {
+onClickSave(pomoValue : string, shortValue : string, longValue : string): void {
+  if (this.errorMessage != "") {
+    console.log("There was at least one error");
+    this.isfucked = !this.isfucked;
+  //  angry wiggle soon
+  } else {
       this.timer.userDurationMsPomodoro(parseInt(pomoValue)*60*1000);
       this.timer.userDurationMsShort(parseInt(shortValue)*60*1000);
       this.timer.userDurationMsLong(parseInt(longValue)*60*1000);
       console.log("The timer settings have been updated")
       this.dialogRef.close();
+
       if (this.timer.currentType == TimerType.pomodoro) {
         this.timer.currentTimeMs = this.timer.durationMsPomodoro;
       } else if (this.timer.currentType == TimerType.short) {
-        this.timer.currentTimeMs = this.timer.durationMsShort;
+         this.timer.currentTimeMs = this.timer.durationMsShort;
       } else {
-        this.timer.currentTimeMs = this.timer.durationMsLong;
+         this.timer.currentTimeMs = this.timer.durationMsLong;
       }
       this.timer.resetTimer();
     }
